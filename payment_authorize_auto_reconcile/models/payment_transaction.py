b176b30f97419e0615a8b5149e462905aad54bd7
Dave Lasley <dave@laslabs.com>
2016-01-12 15:29:57 -0800

    Remove thank you message and begin reconciliation fix

--- payment_authorize_auto_reconcile/models/payment_transaction.py
+++ payment_authorize_auto_reconcile/models/payment_transaction.py
@@ -30,26 +30,6 @@ class PaymentTransaction(models.Model):
     _inherit = 'payment.transaction'
 
     @api.model
-    def _send_thankyou_message(self, pay_amount, invoice):
-        ''' Send a thankyou message to partners subscribed to the invoice  '''
-        last_message = invoice.message_ids[-1]
-        thread = self.env['mail.thread'].browse(last_message.res_id)
-
-        thread.sudo().message_post(
-            subject='Payment Received',
-            body='''
-            Hi %s,
-                <br /><br />
-                Your payment of %s has been received for invoice %s.
-                <br /><br />
-                We appreciate your business!''' % (
-                    invoice.partner_id.name, pay_amount,
-                    invoice.number
-            ),
-            partner_ids=[invoice.partner_id.id]
-        )
-
-    @api.model
     def _authorize_form_get_tx_from_data(self, data):
         """
         Given a data dict coming from authorize, verify it and find the related
@@ -69,20 +49,19 @@ class PaymentTransaction(models.Model):
             raise ValidationError(error_msg)
 
         tx = self.search([('reference', '=', reference)])
+        invoice = self.env['account.invoice'].search([
+            ('number', '=', reference)
+        ], limit=1)
+        acquirer = self.env['payment.acquirer'].search([
+            ('provider', '=', 'Authorize.Net'),
+            ('company_id', '=', invoice.company_id.id)
+        ], limit=1)
+
+        pay_amount = data.get('x_amount')
 
         if not tx:
 
-            invoice = self.env['account.invoice'].search([
-                ('internal_number', '=', reference)
-            ], limit=1)
-            acquirer = self.env['payment.acquirer'].search([
-                ('provider', '=', 'authorize'),
-                ('company_id', '=', invoice.company_id.id)
-            ], limit=1)
-
-            pay_amount = data.get('x_amount')
-
-            tx = [self.create({
+            tx_vals = {
                 'reference': reference,
                 'acquirer_id': acquirer.id,
                 'amount': pay_amount,
@@ -93,21 +72,9 @@ class PaymentTransaction(models.Model):
                 'partner_state': data.get('x_state'),
                 'partner_city': data.get('x_city'),
                 'partner_street': data.get('x_address'),
-            })]
-
-            trans_id = data.get('x_trans_id', 0)
-            invoice.pay_and_reconcile(
-                pay_amount=pay_amount,
-                pay_account_id=invoice.account_id.id,
-                period_id=invoice.period_id.id,
-                pay_journal_id=invoice.journal_id.id,
-                writeoff_acc_id=invoice.account_id.id,
-                writeoff_period_id=invoice.period_id.id,
-                writeoff_journal_id=invoice.journal_id.id,
-                name='Authorize.net Transaction ID %s' % trans_id,
-            )
-
-            # self._send_thankyou_message(pay_amount, invoice)
+            }
+            _logger.debug('Creating tx with %s', tx_vals)
+            tx = [self.create(tx_vals)]
 
         elif len(tx) > 1:
             error_msg = 'Authorize: received data for reference %s' % (
@@ -116,5 +83,18 @@ class PaymentTransaction(models.Model):
             error_msg += '; multiple orders found'
             _logger.error(error_msg)
             raise ValidationError(error_msg)
+
+        trans_id = data.get('x_trans_id', 0)
+        _logger.debug('Paying %s on %s', pay_amount, invoice)
+        invoice.pay_and_reconcile(
+            pay_amount=pay_amount,
+            pay_account_id=invoice.account_id.id,
+            period_id=invoice.period_id.id,
+            pay_journal_id=invoice.journal_id.id,
+            writeoff_acc_id=invoice.account_id.id,
+            writeoff_period_id=invoice.period_id.id,
+            writeoff_journal_id=invoice.journal_id.id,
+            name='Authorize.net Transaction ID %s' % trans_id,
+        )
 
         return tx[0]
