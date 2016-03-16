774792fa4b1940e3db1fac7b9ee81bc0523fbcf6
Dave Lasley <dave@laslabs.com>
2016-03-16 07:44:06 -0700

    Switch generic ValidationError to import from payment_provider in payment_paypal_auto_reconcile

--- payment_paypal_auto_reconcile/models/payment_transaction.py
+++ payment_paypal_auto_reconcile/models/payment_transaction.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, api, fields
-from openerp.exceptions import ValidationError
+from openerp.addons.payment.models.payment_acquirer import ValidationError
 import logging
 
 
